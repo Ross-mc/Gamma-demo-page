368cd17f32cb415bd427215eeb372f1cc2c97044
Ross McIntyre <rossmci90@hotmail.co.uk>
2020-12-15 20:21:29 +0000

    Added basic animations

--- Assets/script.js
+++ Assets/script.js
@@ -18,10 +18,25 @@ const toggleBtnHandler = () => {
     var circleDiv = document.querySelector(".circle");
     if (switchOff) {
         switchDiv.style.backgroundColor = "#7653f3";
-
+        var marginLeft = 5;
+        var animation = setInterval(function(){
+            marginLeft+= 1;
+            circleDiv.style.marginLeft = marginLeft + "%";
+            if (marginLeft === 55){
+                clearInterval(animation)
+            }
+        }, 1)
         switchOff = false;
     } else{
         switchDiv.style.backgroundColor = "#cacee9";
+        var marginLeft = 50;
+        var animation = setInterval(function(){
+            marginLeft-= 1;
+            circleDiv.style.marginLeft = marginLeft + "%";
+            if (marginLeft === 5){
+                clearInterval(animation)
+            }
+        }, 1)
         switchOff = true;
     }
     
